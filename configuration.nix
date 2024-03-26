2fe5b5afc972a7438ff999e2d48180d44c3b9eb9
josh-reynolds <josh.reynolds@frontier.com>
2024-03-26 14:14:38 -0400

    Adding Steam to nixOS configuration

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
+  programs.steam.enable = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
